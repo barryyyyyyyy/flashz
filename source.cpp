59566daf7ad012458e9bb67542ce47db733be9c8
barry <73419491+barryyyyyyyy@users.noreply.github.com>
2020-10-26 20:01:30 +0530

    Create source.cpp

---
+++ source.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+ int x,n,i,j=1;
+ float a,b,c,d;
+ clrscr();
+ cout<<"Select a function to operate_\n\n1. Addition\n2. Substraction\n3. Multiplication\n4. Division\n5. Factorial\n6. Percentage\n7. Power\n8. Square_Root\n";
+ cin>>x;
+ switch(x)
+ {
+   case 1:cout<<"Enter the numbers  ";
+	  cin>>a>>b;
+	  cout<<"The result is_"<<a+b;
+	  break;
+   case 2:cout<<"Enter the greater number  ";
+	  cin>>a;
+	  cout<<"Enter the smaller number  ";
+	  cin>>b;
+	  cout<<"The result is_"<<a-b;
+	  break;
+   case 3:cout<<"Enter he numbers  ";
+	  cin>>a>>b;
+	  cout<<"The result is_"<<a*b;
+	  break;
+   case 4:cout<<"Enter Dividend  ";
+	  cin>>a;
+	  cout<<"Enter Divisor  ";
+	  cin>>b;
+	  cout<<"The result is_"<<a/b;
+	  break;
+   case 5:cout<<"Enter the number ";
+	  cin>>n;
+	  for(i=n;i>0;i--)
+	   {
+	     j=j*i;
+	    }
+	  cout<<"The factorial of "<<n<<"is_"<<j;
+	  break;
+   case 6:cout<<"Enter percent value  ";
+	  cin>>a;
+	  cout<<"Enter operand  ";
+	  cin>>b;
+	  cout<<"The result is_"<<(a*b)/100;
+	  break;
+   case 7:cout<<"Enter base  ";
+	  cin>>a;
+	  cout<<"Enter power  ";
+	  cin>>b;
+	  cout<<"The result is_"<<pow(a,b);
+	  break;
+   case 8:cout<<"Enter the number  ";
+	  cin>>a;
+	  cout<<"The result is_"<<sqrt(a);
+	  break;
+  }
+  cout<<"\n\n\nNothing more :)";
+  getch();
+}
